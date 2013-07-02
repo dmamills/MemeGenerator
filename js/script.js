d3b7e01cda9a0c6918d318e8bcd1ea3aa027309d
Daniel Mills <ohshuckabees@gmail.com>
2013-07-02 01:03:28 -0400

    some basic css styling

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+    		var imgEl;
+
+    		function ctrl($scope) {
+
+	    			$scope.toptext = '';
+	    			$scope.bottomtext = '';
+	    			$scope.selectedMeme ='10 Guy';
+	    			$scope.download = '#';
+
+	    			$scope.alterMeme = function(){
+	    				
+	    				console.meme($scope.toptext,$scope.bottomtext,$scope.selectedMeme);	
+	    				
+	    			}
+	    			$scope.createImage = function(){
+
+	    				var canvas = document.getElementById('myCanvas');
+	    				$scope.download = canvas.toDataURL();
+
+						if(imgEl.attr('src') == '#')
+							imgEl.show();
+	    				}
+	    		}
+	
+			$(document).ready(function(){
+
+				var memeList = document.getElementById('meme-list');
+
+				for(var meme in console.list){
+					$('#meme-list').append('<option>'+meme+'</option>');
+				}
+					memeList.selectedIndex = 1;
+					console.meme('','','10 Guy');	
+				 	
+				 	imgEl = $('#imgtest');
+					if(imgEl.attr('src') == '#')
+						imgEl.hide();
+
+			});
