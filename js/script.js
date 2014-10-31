61b3656f27a00c81da83754e556e09238cbb2cc3
Daniel Mills <mills.dma@gmail.com>
2014-10-31 18:06:25 -0400

    lolsoold

--- js/script.js
+++ js/script.js
@@ -1,44 +1,41 @@
-function ctrl($scope) {
-		$scope.toptext = '';
-		$scope.bottomtext = '';
-		$scope.selectedMeme ='10 Guy';
-		$scope.alterMeme = function(){
-			console.meme($scope.toptext,$scope.bottomtext,$scope.selectedMeme);		
+angular.module('Meme',[])
+.controller('MainController',function($scope) {
+
+	$scope.toptext = '';
+	$scope.bottomtext = '';
+	$scope.selectedMeme ='10 Guy';
+	$scope.memes = [];
+
+	for(var key in console.list) {
+		$scope.memes.push({name:key,url:console.list[key]});
+	}
+
+	$scope.alterMeme = function(){
+		var url = $scope.memes[$scope.memes.indexOf($scope.selectedMeme)].name;
+		console.meme($scope.toptext,$scope.bottomtext,url);
+	}
+
+	$scope.create = function() {
+		var uri = document.getElementById('myCanvas').toDataURL();
+		downloadImage(uri,generateName());
+	}
+
+	function generateName() {
+		var n = [];
+		for(var i =0; i < 10;i++) {
+			n.push((Math.floor(Math.random() *16)).toString(16));
 		}
-}
+		return n.join('');
+	};
 
+	function downloadImage(uri,name) {
+		var link = document.createElement('a');
+		link.download = name;
+		link.href = uri;
+		link.click();
+	};
 
-function create() {
-	var uri = document.getElementById('myCanvas').toDataURL();
-	downloadImage(uri,generateName());
-}
-
-function generateName() {
-	var n = [];
-	for(var i =0; i < 10;i++) {
-		n.push((Math.floor(Math.random() *16)).toString(16));
-	}
-	return n.join('');
-}
-
-function downloadImage(uri,name) {
-	var link = document.createElement('a');
-	link.download = name;
-	link.href =uri;
-	link.click();
-}
-
-
-$(function() {
-	var memeListEl = document.getElementById('meme-list');
-
-	for(var meme in console.list) {
-		var optionEl = document.createElement('option');
-		optionEl.value = optionEl.innerText = meme;
-		memeListEl.appendChild(optionEl);
-	}
-		memeListEl.selectedIndex = 1;
-		console.meme('','','10 Guy');
-	
-	document.getElementById('createBtn').addEventListener('click',create);
+})
+.run(function() {
+	console.meme('','','10 Guy');
 });
